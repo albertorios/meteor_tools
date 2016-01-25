7b564523897a347edbd20a5fc54db935b4b2b408
Alberto Rios <albertorios94@gmail.com>
2016-01-24 21:41:24 -0600

    using wget instead of cp

--- meteor_setup.sh
+++ meteor_setup.sh
@@ -4,8 +4,9 @@ mkdir $1/server $1/client $1/public $1/private
 mkdir $1/client/lib
 mkdir $1/client/lib/semantic
 touch $1/client/lib/semantic/custom.semantic.json
-cp setup.py $1/setup.py
+#cp setup.py $1/setup.py
 cd $1
+wget https://raw.githubusercontent.com/albertorios/meteor_tools/master/setup.py
 meteor add iron:router semantic:ui flemay:less-autoprefixer fourseven:scss okgrow:router-autoscroll
 python setup.py
 atom ./
