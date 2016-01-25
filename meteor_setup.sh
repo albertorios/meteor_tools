04426dc9f5955a2b132eb7019093addbe999dd1a
Alberto Rios <albertorios94@gmail.com>
2016-01-25 02:34:29 -0600

    Changed stuff so that I can wget one script and just run that to get everything else

--- meteor_setup.sh
+++ meteor_setup.sh
@@ -4,9 +4,16 @@ mkdir $1/server $1/client $1/public $1/private
 mkdir $1/client/lib
 mkdir $1/client/lib/semantic
 touch $1/client/lib/semantic/custom.semantic.json
-#cp setup.py $1/setup.py
 cd $1
 wget https://raw.githubusercontent.com/albertorios/meteor_tools/master/setup.py
+wget https://raw.githubusercontent.com/albertorios/meteor_tools/master/db.js
+wget https://raw.githubusercontent.com/albertorios/meteor_tools/master/create_survey_db.py
+wget https://raw.githubusercontent.com/albertorios/meteor_tools/master/create_survey_db.sh
+wget https://raw.githubusercontent.com/albertorios/meteor_tools/master/db_template
+wget https://raw.githubusercontent.com/albertorios/meteor_tools/master/owners
+mv owners private/owners
+mv db_template server/db_template
+chmod 755 create_survey_db.sh
 meteor add iron:router semantic:ui flemay:less-autoprefixer fourseven:scss okgrow:router-autoscroll
 python setup.py
 atom ./
