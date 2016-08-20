ee6109ffab3b588dafe28449437dfc1168c21e08
Alberto Rios <albertorios94@gmail.com>
2016-08-19 19:25:01 -0500

    add session

--- meteor_setup.sh
+++ meteor_setup.sh
@@ -18,9 +18,8 @@ wget https://raw.githubusercontent.com/albertorios/meteor_tools/master/owners
 mv owners private/owners
 mv db_template server/db_template
 chmod 755 create_survey_db.sh
-meteor add iron:router semantic:ui flemay:less-autoprefixer fourseven:scss accounts-password accounts-ui
+meteor add iron:router semantic:ui flemay:less-autoprefixer fourseven:scss  accounts-password accounts-ui okgrow:router-autoscroll session
 meteor remove autopublish insecure
-okgrow:router-autoscroll
 python setup.py
 atom ./
 meteor
